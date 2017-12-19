f2a2fe6054850ee21a5d39e1339d4a4221f47ad9
jenniferneale <jeneale6@gmail.com>
2017-12-19 10:38:51 -0500

    Fixed export problem

--- client/components/getpercentile.js
+++ client/components/getpercentile.js
@@ -1,37 +1,63 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import { getUserById } from '../store'
-import { SingleUserLookup } from './singleUserLookup';
 
-export const GetPercentile = (props) => {
-    const {handleSubmit} = props;
-    console.log('props', props);
-    return (
-    <div>
-      <h3>Get the Percentile for Your Candidate Id:</h3>
-      <form onSubmit={handleSubmit} name="GetPercentile">
-        <div>
-            <label htmlFor="candidateId"><small>Candidate Id</small></label>
-            <input name="candidateId" type="text" />
-        </div>
-        <div>
-            <button type="submit">Submit</button>
-        </div>
-    </form>
-    <SingleUserLookup />
-    </div>
-    )
+const GetPercentile = props => {
+    
+        return (<div>
+              <h3>Get the Percentile for Your Candidate Id:</h3>
+              <form onSubmit={props.handleSubmit} name="GetPercentile">
+                <div>
+                    <label htmlFor="candidateId"><small>Candidate Id</small></label>
+                    <input name="candidateId" type="text" />
+                </div>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+            <div>
+            {props.user.id && 
+                <div>
+                    <h3>Candidate No.{props.user.id}</h3>
+                    <table className="table table-striped table-hover col-sm-11">
+                        <tbody>
+                            <tr>
+                                <td>Title</td>
+                                <td>{props.user.title}</td>                                
+                            </tr>
+                            <tr>
+                                <td>Communication Score</td>
+                                <td>{props.user.communication_score}</td>
+                                <td>75th percentile</td>
+                            </tr>
+                            <tr>
+                                <td>Coding Score</td>
+                                <td>{props.user.coding_score}</td>
+                                <td>75th percentile</td>
+                            </tr>                            
+                        </tbody>
+                    </table>
+                </div>
+            }      
+            </div>
+            </div>)    
+
 }
+
+const mapState = (state) => {
+    return {
+        user: state.userLookup.user
+      }
+  }
 
 const mapDispatch = (dispatch) => {
     return {
         handleSubmit (evt) {
             evt.preventDefault();
             const candidateId = evt.target.candidateId.value;
-            console.log('----------------candidateId ', candidateId);
             dispatch(getUserById(candidateId))
         }
     }
 }
 
-  export default connect(state => state, mapDispatch)(GetPercentile)
+export default connect(mapState, mapDispatch)(GetPercentile)
--- client/components/index.js
+++ client/components/index.js
@@ -6,5 +6,4 @@
 export {default as Main} from './main'
 export {default as UserHome} from './user-home'
 export {Login, Signup} from './auth-form'
-export {GetPercentile} from './getpercentile'
-export {SingleUserLookup} from './singleUserLookup'
+export {default as GetPercentile} from './getpercentile'
